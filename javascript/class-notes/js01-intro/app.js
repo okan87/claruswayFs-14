a7ccc1ced02c22d729ae078275e40c2b1b646edc
okan87 <okankaplan1907@gmail.com>
2023-05-03 23:00:53 +0200

    introduce

--- javascript/class-notes/js01-intro/app.js
+++ javascript/class-notes/js01-intro/app.js
@@ -1,5 +1,7 @@
-console.log('Yerel kod calisti');
-alert('Bu bir alerttir') /* Alert uygulamayi bloklar*/
-karar = confirm('Emin misiniz? E/H')
-console.log(karar);
+console.log('Yerel kod calisti')
+// alert('Bu bir alerttir') /* Alert uygulamayi bloklar*/
+// karar = confirm('Emin misiniz? E/H')
+// console.log(karar);
 console.log('Alertten sonraki komut');
+console.warn('Bu bir uyaridir')
+console.error('Bu bir hatadir')
