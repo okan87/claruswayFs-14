7f8124cd9567638983405da7aa07bab21c46c6f0
okan87 <okankaplan1907@gmail.com>
2023-05-03 22:06:32 +0200

    Netflix project is finished

---
+++ javascript/class-notes/js01-intro/app.js
@@ -0,0 +1,5 @@
+console.log('Yerel kod calisti');
+alert('Bu bir alerttir') /* Alert uygulamayi bloklar*/
+karar = confirm('Emin misiniz? E/H')
+console.log(karar);
+console.log('Alertten sonraki komut');
