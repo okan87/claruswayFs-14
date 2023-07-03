1cb627e4965a17ad5c881a6c3c26792bce0c2761
okan87 <okankaplan1907@gmail.com>
2023-07-03 23:05:38 +0200

    update

---
+++ react/class-notes/rc03_sass/src/App.js
@@ -0,0 +1,18 @@
+import Card from "./components/Card"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+
+//? SCSS styleshet'i import ettik
+import "./scss/app.scss"
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Card />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ react/class-notes/rc03_sass/src/components/Card.jsx
@@ -0,0 +1,11 @@
+import CardStyle from "../scss/card.module.scss"
+
+const Card = () => {
+  return (
+    <div>
+      <h2>Card</h2>
+    </div>
+  )
+}
+
+export default Card
